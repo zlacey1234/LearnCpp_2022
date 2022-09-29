1af624c685dee82d446259319bba31505785a000
zlacey1234 <27013004+zlacey1234@users.noreply.github.com>
2022-09-28 17:52:14 -0700

    Section 2 Summary Quiz Question 2

---
+++ Ch-2--Functions-and-Files/Section-2-Summary-Quiz-Question-2/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int ReadNumber() {
+    std::cout << "Enter an integer: ";
+
+    int user_input{ };
+    std::cin >> user_input;
+
+    return user_input;
+}
+
+void WriteAnswer(int answer_value) {
+    std::cout << "The answer is " << answer_value << '\n';
+}
---
+++ Ch-2--Functions-and-Files/Section-2-Summary-Quiz-Question-2/section_2_summary_quiz_question_2.cpp
@@ -0,0 +1,11 @@
+int ReadNumber();
+void WriteAnswer(int answer_value);
+
+int main() {
+    int user_input_1 { ReadNumber() };
+    int user_input_2 { ReadNumber() };
+
+    WriteAnswer( user_input_1 + user_input_2);
+
+    return 0;
+}
