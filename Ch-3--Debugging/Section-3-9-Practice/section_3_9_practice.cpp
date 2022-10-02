317c5edf047dd55a6eef362d22524f18bae58c8a
zlacey1234 <27013004+zlacey1234@users.noreply.github.com>
2022-10-02 12:45:03 -0700

    Section 3.9 Practice

---
+++ Ch-3--Debugging/Section-3-9-Practice/section_3_9_practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void a() {
+    std::cout << "a() called\n";
+}
+
+void b() {
+    std::cout << "b() called\n";
+    a();
+}
+
+int main() {
+    a();
+    b();
+
+    return 0;
+}
